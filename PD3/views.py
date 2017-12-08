d7afbe227199c8b6056221f24b95e4d86ad51267
csucsii <csucsicsucsi@gmail.com>
2017-12-08 14:03:38 +0100

    PC_help

--- PD3/views.py
+++ PD3/views.py
@@ -17,7 +17,7 @@ class Decision(Page):
     form_fields = ['cooperate']
 
     def before_next_page(self):
-        self.player.cooperate_bot = random.choice(Constants.COOPCHOICES)
+        self.player.cooperate_bot = random.choice(False,True)
         self.player.set_payoff()
 
 
@@ -34,7 +34,7 @@ class Results(Page):
 
 class Final(Page):
     def is_displayed(self):
-        return self.round_number == Constants.num_rounds()
+        return self.round_number == Constants.num_rounds
 
     form_model = models.Player
     form_fields = ['final_satisfaction']
